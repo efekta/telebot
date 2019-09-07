364e0a2e430aa9141919c1a845dadd1da5466913
Ptashkina Liliya <efekta9@gmail.com>
2019-09-07 18:11:24 +0300

    user block, content template

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
     svg4everybody({});
+    $('.box-drop-down').on('click', function() {
+    	$('.user-cab').toggleClass('user-cab_show');
+    })
+    // $('.box-drop-down').on('click', function() {
+    // 	$($(this)).toggleClass('box-drop-down_up');
+    // })
+    $('.close-ico').on('click', function() {
+    	$('.user-cab').removeClass('user-cab_show');
+    	// if ($(body).hasClass('user-cab_show')) {
+    	// 	$('.box-drop-down').removeClass('box-drop-down_up');
+    	// };
+    })
 });
+
+
